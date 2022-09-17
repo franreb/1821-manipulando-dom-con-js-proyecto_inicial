c933e341f9dfe4b71b7143806ed45f65c59cd2de
Franco <francorebechi_@hotmail.com>
2022-09-16 22:18:47 -0300

    Clase 05 Terminada

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( () => {
+import checkComplete from "./component/completeTask.js";
+import deleteIcon from "./component/deleteIcon.js";
+
 const btn= document.querySelector('[data-form-btn]');
 
 
@@ -17,28 +19,16 @@ const createTask = (evento) => {
     tittleTask.classList.add("task");
     tittleTask.innerText = value;
     taskContent.appendChild(tittleTask);
+    //taskContent.appendChild(deleteIcon());
     const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
     //task.innerHTML = content;
     
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 }
 
 btn.addEventListener('click', createTask);
 
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click", completeTask);
-    return i;
-}
 
-const completeTask = (event) =>  {
-    const element = event.target
-
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-}
-})();
